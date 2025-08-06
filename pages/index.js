a55f22d9ac16aa78371b8f8ec72fc91c4501c652
Dimitri Viana Pedroni <135239573+Dimitri-VP@users.noreply.github.com>
2025-08-06 18:11:52 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Next.js no Codespaces!</h1>
+      <p>Este é um projeto inicial configurado para desenvolvimento na nuvem.</p>
+    </div>
+  );
+}
